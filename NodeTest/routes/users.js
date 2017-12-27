cd934102cdcfda8c355e2fb06dc6a872173be379
tengxing <tengxing7452@163.com>
2017-12-27 14:43:37 +0800

    在routes引入dao

--- NodeTest/routes/users.js
+++ NodeTest/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
- var URL = require('url');  
+var URL = require('url');
+var userDao = require('../dao/userDao');
 
 function User() {
       this.name;
@@ -8,33 +9,37 @@ function User() {
       this.age;
 }
 
- router.get('/getUserInfo', function(req, res, next) {
+router.get('/getUserInfo', function(req, res, next) {
 
     var user = new User();
     console.info("exe");
     var params = URL.parse(req.url, true).query;
-  
+
  if(params.id == '1') {
 
     user.name = "ligh";
     user.age = "1";
     user.city = "北京市";
 
-}else{  
+}else{
     user.name = "SPTING";
     user.age = "1";
     user.city = "杭州市";
 }
 
   var response = {status:1,data:user};
-  res.send(JSON.stringify(response));
-
+  res.send(JSON.parse(JSON.stringify(response)));
 });
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({status:'json'});
+});
+
+/* GET users listing. */
+router.get('/blog', function(req, res, next) {
+  userDao.list(req, res, next);
 });
 
 module.exports = router;
