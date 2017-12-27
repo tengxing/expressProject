23f557aae94def8a9047c4bcb0d53ba50efc0516
tengxing <tengxing7452@163.com>
2017-12-27 16:12:18 +0800

    思考感悟

--- NodeTest/dao/userDao.js
+++ NodeTest/dao/userDao.js
@@ -19,9 +19,21 @@ useConnectionPooling: true
 var  sql = "SELECT count(*) count,DATE_FORMAT(releaseDate,'%Y年%m月') date FROM yjxx.article group by DATE_FORMAT(releaseDate,'%Y年%m月') ";
 
 
+var $db = require('../config/db');
+var pool = $db.connect();
+
+
 module.exports = {
+    //node不会在第一个的sql执行的时候等待,直接执行下面的console.info("getBlogCount logger------>");
+    //由于使用res只有一个，所以不管谁先用，会导致后面的一个res不能使用，因为已经返回了，除非建立长连接(没有测试过)
+    //如果要执行多个sql,解决可以考虑使用方法嵌套。
     list: function(req, res) {
-       console.info("getBlogCount logger------>");
+      var result;
+      $db.executeSql(sql, [], function(err, result) {
+          //res.json({"result":result});
+          console.log(result);
+      },pool);
+      console.info("getBlogCount logger------>");
        //查
        connection.query(sql,function (err, result) {
         if(err){
@@ -29,9 +41,10 @@ module.exports = {
           return;
         }
         console.log(result);
+        result1 = result
         //把搜索值输出
-       res.set('Content-Type', 'application/json')
-       res.send({"result":result});
+       //res.set('Content-Type', 'application/json')
+       res.send({"result1":result1});
        });
     }
 }
